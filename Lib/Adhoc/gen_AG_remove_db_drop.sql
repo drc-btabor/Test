63fa917acb8486499dd6b284ccfba067de214a82
Tabor <btabor@datarecognitioncorp.com>
2018-04-16 12:12:30 -0500

    remove listner

---
+++ Lib/Adhoc/gen_AG_remove_db_drop.sql
@@ -0,0 +1,33 @@
+/***** Do Not Run Blindly ****
+* The Avamar agent should be removed from ag roles prior to executing sql (else remove listener will fail)
+* Run command one by one, instead of big batch due to no error_handling
+* Verify each command completion prior to next command
+* SQL will generate sql to remove db from ag, remove listener and drop AG
+*
+**********************************************************************/
+
+set nocount on
+declare @sql varchar(max) =''
+
+--check if primary
+if not exists( select 1 from sys.dm_hadr_availability_group_states where primary_replica =@@SERVERNAME )
+      goto error_handler
+
+select @sql=coalesce(@sql + char(10),'') + ' 
+ALTER AVAILABILITY GROUP ['+a.name+'] REMOVE DATABASE ['+database_name+'];
+waitfor delay ''00:00:15''
+exec dba_analysis..dba_kill_all @db_n='''+database_name+'''
+alter database '+database_name+ ' set offline'
+from sys.availability_groups a
+inner join sys.availability_databases_cluster b
+on a.group_id = b.group_id
+
+print @sql
+
+goto eof
+
+error_handler:
+    raiserror ('Must be ran on Primary Replcia', 16,1)
+
+eof:
+go
